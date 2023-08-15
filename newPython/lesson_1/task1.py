96502c6a88718a5eb23fb5a1a58c93ab033c7760
Grigor25 <93980197+Grigor25@users.noreply.github.com>
2023-08-15 18:23:09 +0300

    Add files via upload

---
+++ newPython/lesson_1/task1.py
@@ -0,0 +1,23 @@
+print('Grigor')
+
+current_year = 2023
+current_age = 28
+print(current_year - current_age)
+
+full_name = "Manukyan"
+age = 28
+print(full_name,age)
+
+vars = 'string,number,boolean,NoneType  age = 10 '
+print(vars)
+
+x = 16
+y = 5
+z = x // y == 3
+print(type(z))
+
+
+#Reasearch
+#1 % veradarcnum e mnacord@
+#2 * oprator@ stringi vra kirarelis Error e talis
+#3 voch
