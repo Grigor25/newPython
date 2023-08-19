8ebe6e6ed9c9e5aedeb82018cdabf4662998c26c
Grigor25 <93980197+Grigor25@users.noreply.github.com>
2023-08-19 20:30:31 +0300

    Add files via upload

--- newPython/lesson_1/task1.py
+++ newPython/lesson_1/task1.py
@@ -8,7 +8,7 @@ full_name = "Manukyan"
 age = 28
 print(full_name,age)
 
-vars = 'string,number,boolean,NoneType  age = 10 '
+vars = 'string,number,boolean,NoneType  age = 10 new_age = 15 newAge = 15'
 print(vars)
 
 x = 16
@@ -19,5 +19,5 @@ print(type(z))
 
 #Reasearch
 #1 % veradarcnum e mnacord@
-#2 * oprator@ stringi vra kirarelis Error e talis
+#2 * ete kirarenq tvi het string@ etqan angam kbazmapatki
 #3 voch
