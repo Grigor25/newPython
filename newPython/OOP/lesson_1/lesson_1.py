80c9fb70c4feae030afb95e60dbcbaa51da17ac8
Grigor25 <93980197+Grigor25@users.noreply.github.com>
2023-09-23 20:33:19 +0300

    Add files via upload

---
+++ newPython/OOP/lesson_1/lesson_1.py
@@ -0,0 +1,38 @@
+class Car:
+    def __init__(self,brand, model, color, year, started=False):
+        self.brand = brand
+        self.model = model
+        self.color = color
+        self.year = year
+        self.started = started
+    def start(self):
+        print('br br br')
+        self.started = True
+        print(self.__dict__)
+
+    def brand_modle(self):
+        print(self.model,self.brand)
+        return  self.brand + self.model
+
+    def stop(self):
+        print('stop')
+        self.started = False
+
+    def model_year(self):
+        return 2023 - self.year
+
+    def color_change(self,color):
+        self.color = color
+
+bmv = Car('Bmw','X5','Red','2010')
+hyundai = Car('Hyundayi','Elantra','Black','2014')
+bmv.color_change('Grey')
+
+print(bmv.start())
+
+
+
+#Reserch
+
+#constructor@ method e vor@ classic object setextselu jamanak e ashxatum,stextsum e  clasi stat@ __init__
+#destructor method e vorch@ jnjum e objecti stat@ __del__
---
+++ newPython/OOP/lesson_2/lesson_2.py
@@ -0,0 +1,55 @@
+class Car:
+    headlights_count = 4
+    def __init__(self,horsepower, weight, engine_type, wheels_count):
+        self.horsepower = horsepower
+        self.weight = weight
+        self.engine_type = engine_type
+        self.wheels_count = wheels_count
+
+    def max_speed(self):
+        return self.horsepower / self.weight * 1500
+
+    staticmethod
+    def weight_change(self,weigth):
+        self.weight = weigth
+        print(self.__dict__)
+    # classmethod
+    # def weight_change(new_weight):
+    #     weight = new_weight
+
+
+class Bus(Car):
+    def __init__(self,horsepower, weight, engine_type,wheels_count,max_seats,busy_seats=0):
+        self.horsepower = horsepower
+        self.weight = weight
+        self.engine_type = engine_type
+        self.wheels_count = wheels_count
+        self.max_seats = max_seats
+        self.busy_seats = busy_seats
+
+    def add_busy_sets(self):
+        self.busy_seats += 1
+
+    def free_seats(self):
+        return self.max_seats - self.busy_seats
+
+
+bmw = Car(1500, 200, 8, 4)
+opel = Car(400, 50, 20, 4)
+busm  = Bus(500, 20, 20, 20, 40)
+
+
+print(busm.free_seats())
+busm.add_busy_sets()
+print(dir(busm))
+print(busm.free_seats())
+print(busm.max_speed())
+
+print(bmw.max_speed(),opel.max_speed())
+
+
+
+#Reasearch
+
+
+# __dict__ @ cuyc e talis objecti atributner@ __initov__trvats  isk dir funcian cuyc e talis classi ev objecti @ndhanur bolor atributner@ ev methodner@
