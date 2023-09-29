bcb74dcd21403af47946a9aa6fd5fbc8df670c57
Grigor25 <93980197+Grigor25@users.noreply.github.com>
2023-09-29 15:40:30 +0300

    Add files via upload

--- newPython/OOP/lesson_2/lesson_2.py
+++ newPython/OOP/lesson_2/lesson_2.py
@@ -1,6 +1,7 @@
 class Car:
     headlights_count = 4
-    def __init__(self,horsepower, weight, engine_type, wheels_count):
+
+    def __init__(self, horsepower, weight, engine_type, wheels_count):
         self.horsepower = horsepower
         self.weight = weight
         self.engine_type = engine_type
@@ -9,17 +10,17 @@ class Car:
     def max_speed(self):
         return self.horsepower / self.weight * 1500
 
-    staticmethod
-    def weight_change(self,weigth):
+    @staticmethod
+
+    def weight_change(self, weigth):
         self.weight = weigth
-        print(self.__dict__)
-    # classmethod
+    # @classmethod
     # def weight_change(new_weight):
     #     weight = new_weight
 
 
 class Bus(Car):
-    def __init__(self,horsepower, weight, engine_type,wheels_count,max_seats,busy_seats=0):
+    def __init__(self, horsepower, weight, engine_type, wheels_count, max_seats, busy_seats=0):
         self.horsepower = horsepower
         self.weight = weight
         self.engine_type = engine_type
@@ -36,8 +37,7 @@ class Bus(Car):
 
 bmw = Car(1500, 200, 8, 4)
 opel = Car(400, 50, 20, 4)
-busm  = Bus(500, 20, 20, 20, 40)
-
+busm = Bus(500, 20, 20, 20, 40)
 
 print(busm.free_seats())
 busm.add_busy_sets()
@@ -45,11 +45,10 @@ print(dir(busm))
 print(busm.free_seats())
 print(busm.max_speed())
 
-print(bmw.max_speed(),opel.max_speed())
+print(bmw.max_speed(), opel.max_speed())
+
+# Reasearch
 
 
-
-#Reasearch
-
-
-# __dict__ @ cuyc e talis objecti atributner@ __initov__trvats  isk dir funcian cuyc e talis classi ev objecti @ndhanur bolor atributner@ ev methodner@
+# __dict__ @ cuyc e talis objecti atributner@ __initov__trvats  isk dir funcian cuyc e talis classi ev objecti
+# @ndhanur bolor atributner@ ev methodner@
