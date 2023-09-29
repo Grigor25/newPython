7ea1636deb8f02c5c799cbb450f8c5bfc06fdb86
Grigor25 <93980197+Grigor25@users.noreply.github.com>
2023-09-29 18:47:27 +0300

    Add files via upload

---
+++ newPython/OOP/lesson_3/lesson_3.py
@@ -0,0 +1,63 @@
+class Animal:
+    """Main Class"""
+    def __init__(self, eyes, hands, lags, color, runing_speed=45):
+        self.eyes = eyes
+        self.hands = hands
+        self.lags = lags
+        self.color = color
+        self.runing_speed = runing_speed
+
+    def animal_voice(self):
+        return 'dzayn em hanum'
+
+    def run(self):
+        print(f'vazum em {self.runing_speed}')
+
+    def __str__(self):
+        return 'Animal'
+
+
+class Cat(Animal):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def animal_cat_voice(self):
+        return f'mlavelu {super().animal_voice()}'
+
+    def cat_voice(self):
+        super().animal_voice()
+        print(self.run())
+
+    def __str__(self):
+        return 'Cat'
+
+
+
+mumu = Cat(5, 2, 2, 2)
+print(mumu.animal_cat_voice())
+print(mumu.__dict__)
+
+
+
+class Lion(Cat):
+    def human_eat(self):
+        print('human eat')
+
+    def __str__(self):
+        return 'Lion'
+print(Animal.__doc__)
+simba = Lion(4, 4, 4, 4)
+print(simba.human_eat())
+
+print(Animal.__bases__)
+print(Cat.__bases__)
+
+
+print(issubclass(Cat, Lion))
+print(issubclass(Lion, Animal))
+
+#Research
+
+#__doc__   informacia e talis classi veraberyal
+#__basse__ cuyc e talis clasi tsnox class@ te vor clasice jarangvum
+#issubclass@ stugum e te arajin parametrov trvats object@ jarangum e erkrordic te voch
